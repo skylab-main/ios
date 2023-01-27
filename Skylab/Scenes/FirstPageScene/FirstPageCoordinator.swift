e99a40701b6041d68986a2400316aa5ad3dcae47
SerhiiMolodets <mr.molodets@gmail.com>
2023-01-27 19:23:45 +0200

    removed delegate, added rx bindings;

--- Skylab/Scenes/FirstPageScene/FirstPageCoordinator.swift
+++ Skylab/Scenes/FirstPageScene/FirstPageCoordinator.swift
@@ -6,52 +6,65 @@
 //
 
 import UIKit
+import RxSwift
+
 
 class FirstPageCoordinator: Coordinator {
-
+    
     // MARK: Properties
-
+    
     let rootController: UINavigationController
-
+    let bag = DisposeBag()
+    
     // MARK: Coordinator
-
+    
     init(_ rootController: UINavigationController, parentCoordinator: Coordinator) {
         self.rootController = rootController
     }
-
+    
     override func start() {
-
+        
         let viewController = FirstPageViewController.instantiate(coordinator: self)
-        viewController.viewModel = FirstPageViewModel()
-        viewController.viewModel?.coordinatorDelegate = self
+        let viewModel = FirstPageViewModel()
+        viewController.viewModel = viewModel
+        //        viewController.viewModel?.coordinatorDelegate = self
+        //        MARK: - Observe to viewModel and call to func openFirstSubcontroller
+        viewModel.didOpenFirstSubController
+            .subscribe(onNext: { [weak self] in self?.openFirstSubcontroller()} )
+            .disposed(by: bag)
+        
+        viewModel.didOpenSecondSubController
+            .subscribe(onNext: { [weak self] in self?.openSecondSubController()} )
+            .disposed(by: bag)
+        
         rootController.tabBarItem = UITabBarItem(title: "First",
                                                  image: nil,
                                                  selectedImage: nil)
         rootController.pushViewController(viewController, animated: false)
     }
-
+    
     func openFirstSubcontroller() {
         let viewController = FirstPageSubViewController.instantiate(coordinator: self)
         viewController.viewModel = FirstPageSubViewControllerViewModel()
         viewController.hidesBottomBarWhenPushed = true
         rootController.pushViewController(viewController, animated: false)
     }
-
+    
     func openSecondSubController() {
-
+        debugPrint("Here will be second navigation func")
     }
 }
 
-extension FirstPageCoordinator: FirstPageViewModelCoordinatorDelegate {
-
-    func openFirstSubControllerDelegate() {
-        self.openFirstSubcontroller()
-    }
-
-    func openSecondSubControllerDelegate() {
-        self.openSecondSubController()
-    }
-}
+//extension FirstPageCoordinator: FirstPageViewModelCoordinatorDelegate {
+//
+//    func openFirstSubControllerDelegate() {
+//        self.openFirstSubcontroller()
+//    }
+//
+//    func openSecondSubControllerDelegate() {
+//        self.openSecondSubController()
+//    }
+//}
 
 
 
