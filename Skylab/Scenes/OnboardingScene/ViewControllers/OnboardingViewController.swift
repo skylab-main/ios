307c743ad6445d1a78d38bb44fc4457f03a10b5b
artembilyy <incomecars4u@gmail.com>
2023-02-16 21:51:17 +0100

    Added viewmodel to onboarding screen.

--- Skylab/Scenes/OnboardingScene/ViewControllers/OnboardingViewController.swift
+++ Skylab/Scenes/OnboardingScene/ViewControllers/OnboardingViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class OnboardingViewController: BaseViewController, Storyboarded {
-
+    var viewModel: OnboardingViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewModel?.openAuthorizationController()
     }
 }
