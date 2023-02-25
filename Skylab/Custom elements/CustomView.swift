99822341b9dc3e78a474af7d122531dd1d8f462b
tk4chenko <tkachhhenko@gmail.com>
2023-02-25 21:24:28 +0200

    Tried to fix a bug with a transparent background.

--- Skylab/Custom elements/CustomView.swift
+++ Skylab/Custom elements/CustomView.swift
@@ -19,23 +19,30 @@ import UIKit
     
     @IBInspectable var distance: CGFloat = 1.0
     
-    @IBInspectable var borderColor: UIColor? {
-        didSet { self.layer.borderColor = borderColor?.cgColor }
+    @IBInspectable var borderColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0) {
+        didSet { self.layer.borderColor = borderColor.cgColor }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        guard let borderColor = self.borderColor else { return }
-        self.addDoubleBorder(borderWidth: borderWidth, borderColor: borderColor.cgColor, cornerRadius: cornerRadius, distance: distance)
+        self.addDoubleBorder()
     }
     
-    func addDoubleBorder(borderWidth: CGFloat, borderColor: CGColor, cornerRadius: CGFloat, distance: CGFloat) {
-        let bottomView = UIView(frame: CGRect(x: self.frame.minX + distance, y: self.frame.minY + distance, width: self.frame.width , height: self.frame.height))
+    func addDoubleBorder() {
+        let bottomView = UIView(frame: CGRect(x: 0 + distance, y: 0 + distance, width: self.frame.width, height: self.frame.height))
         bottomView.layer.borderWidth = borderWidth
-        bottomView.layer.borderColor = borderColor
+        bottomView.layer.borderColor = borderColor.cgColor
         bottomView.layer.cornerRadius = cornerRadius
-        bottomView.backgroundColor = .clear
-        guard let superview = superview else { return }
-        superview.insertSubview(bottomView, belowSubview: self)
+        
+        let maskLayer = CAShapeLayer()
+        let path = UIBezierPath(rect: bottomView.bounds)
+        let maskPath = UIBezierPath(roundedRect: CGRect(x: -distance, y: -distance, width: self.frame.width, height: self.frame.height), cornerRadius: cornerRadius)
+        path.append(maskPath)
+        maskLayer.path = path.cgPath
+        maskLayer.fillRule = .evenOdd
+        
+        bottomView.layer.mask = maskLayer
+        
+        self.addSubview(bottomView)
     }
 }
