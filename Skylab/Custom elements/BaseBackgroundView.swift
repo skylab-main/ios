8af55a595f32447283462b2b8dbcfdd01654eed7
tk4chenko <tkachhhenko@gmail.com>
2023-02-27 20:19:52 +0200

    Added default values for cornerRadius, borderColor, borderWidth, distance and a few lines of comments.

--- Skylab/Custom elements/BaseBackgroundView.swift
+++ Skylab/Custom elements/BaseBackgroundView.swift
@@ -9,31 +9,37 @@ import UIKit
 
 @IBDesignable class BaseBackgroundView: UIView {
     
-    @IBInspectable var borderWidth: CGFloat = 0 {
+//    Sets the width of the border of the view.
+    @IBInspectable var borderWidth: CGFloat = 1 {
         didSet { self.layer.borderWidth = borderWidth }
     }
-    
-    @IBInspectable var cornerRadius: CGFloat = 0 {
+//    Sets the corner radius of the view.
+    @IBInspectable var cornerRadius: CGFloat = 12 {
         didSet { self.layer.cornerRadius = cornerRadius }
     }
+//    Sets the distance between the two borders of the view.
+    @IBInspectable var distance: CGFloat = 6
     
-    @IBInspectable var distance: CGFloat = 0
-    
-    @IBInspectable var borderColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0) {
+//    Sets the color of the border of the view.
+    @IBInspectable var borderColor: UIColor = UIColor(red: 16/255, green: 68/255, blue: 153/255, alpha: 1) {
         didSet { self.layer.borderColor = borderColor.cgColor }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        self.layer.borderColor = self.borderColor.cgColor
+        self.layer.borderWidth = self.borderWidth
+        self.layer.cornerRadius = self.cornerRadius
         self.addDoubleBorder()
     }
     
     func addDoubleBorder() {
+//     Сreates a new UIView instance called bottomView.
         let bottomView = UIView(frame: CGRect(x: distance, y: distance, width: self.frame.width, height: self.frame.height))
         bottomView.layer.borderWidth = borderWidth
         bottomView.layer.borderColor = borderColor.cgColor
         bottomView.layer.cornerRadius = cornerRadius
-        
+//    Removes needless lines of bottomView using a mask.
         let maskLayer = CAShapeLayer()
         let path = UIBezierPath(rect: bottomView.bounds)
         let maskPath = UIBezierPath(roundedRect: CGRect(x: -distance, y: -distance, width: self.frame.width, height: self.frame.height), cornerRadius: cornerRadius)
@@ -41,7 +47,6 @@ import UIKit
         maskLayer.path = path.cgPath
         maskLayer.fillRule = .evenOdd
         bottomView.layer.mask = maskLayer
-        
         self.addSubview(bottomView)
     }
 }
