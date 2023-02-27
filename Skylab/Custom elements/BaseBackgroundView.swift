e9e978931b519af513c84108e6cc984ffa058dc5
tk4chenko <tkachhhenko@gmail.com>
2023-02-27 22:16:58 +0200

    Added default values for cornerRadius, borderColor, borderWidth, distance and a few lines of comments.

--- Skylab/Custom elements/BaseBackgroundView.swift
+++ Skylab/Custom elements/BaseBackgroundView.swift
@@ -7,33 +7,52 @@
 
 import UIKit
 
-@IBDesignable class BaseBackgroundView: UIView {
+@IBDesignable
+final class BaseBackgroundView: UIView {
     
-    @IBInspectable var borderWidth: CGFloat = 0 {
+    // MARK: Inspectable Properties
+
+    // These are inspectable properties that can be set in Interface Builder
+    @IBInspectable var borderWidth: CGFloat = 1 {
         didSet { self.layer.borderWidth = borderWidth }
     }
-    
-    @IBInspectable var cornerRadius: CGFloat = 0 {
+
+    @IBInspectable var cornerRadius: CGFloat = 12 {
         didSet { self.layer.cornerRadius = cornerRadius }
     }
-    
-    @IBInspectable var distance: CGFloat = 0
-    
-    @IBInspectable var borderColor: UIColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0) {
+
+    @IBInspectable var distance: CGFloat = 6
+
+    @IBInspectable var borderColor: UIColor = UIColor(red: 16/255, green: 68/255, blue: 153/255, alpha: 1) {
         didSet { self.layer.borderColor = borderColor.cgColor }
     }
-    
+
+    // MARK: View Lifecycle
+
+    // This method is called whenever the view's layout changes
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.addDoubleBorder()
+        setDefaultLayer()
+        addDoubleBorder()
+    }
+
+    // MARK: Private Methods
+
+    // This private method sets the default layer properties
+    private func setDefaultLayer() {
+        self.layer.cornerRadius = self.cornerRadius
+        self.layer.borderWidth = self.borderWidth
+        self.layer.borderColor = borderColor.cgColor
     }
     
-    func addDoubleBorder() {
+    // This private method adds the double border to the view
+    private func addDoubleBorder() {
+        // Create a new subview to hold the bottom border
         let bottomView = UIView(frame: CGRect(x: distance, y: distance, width: self.frame.width, height: self.frame.height))
         bottomView.layer.borderWidth = borderWidth
         bottomView.layer.borderColor = borderColor.cgColor
         bottomView.layer.cornerRadius = cornerRadius
-        
+        // Create a mask layer to subtract the rounded corners from the bottom view
         let maskLayer = CAShapeLayer()
         let path = UIBezierPath(rect: bottomView.bounds)
         let maskPath = UIBezierPath(roundedRect: CGRect(x: -distance, y: -distance, width: self.frame.width, height: self.frame.height), cornerRadius: cornerRadius)
@@ -41,7 +60,7 @@ import UIKit
         maskLayer.path = path.cgPath
         maskLayer.fillRule = .evenOdd
         bottomView.layer.mask = maskLayer
-        
+        // Add the bottom view as a subview of the main view
         self.addSubview(bottomView)
     }
 }
