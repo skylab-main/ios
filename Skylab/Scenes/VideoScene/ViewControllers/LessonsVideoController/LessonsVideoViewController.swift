c434fdcb9436e5c3df42b53529af431efcff712a
SerhiiMolodets <mr.molodets@gmail.com>
2023-07-07 16:06:50 +0300

    configured UI in LessonsVideoViewController;

--- Skylab/Scenes/VideoScene/ViewControllers/LessonsVideoController/LessonsVideoViewController.swift
+++ Skylab/Scenes/VideoScene/ViewControllers/LessonsVideoController/LessonsVideoViewController.swift
@@ -12,9 +12,27 @@ class LessonsVideoViewController: UIViewController, Storyboarded {
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var telegramButton: UIButton!
-    
+    @IBOutlet weak var lessonThemeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        videoContainerView.layer.borderWidth = 1
+        videoContainerView.layer.borderColor = UIColor.primary.cgColor
+        videoContainerView.layer.cornerRadius = 12
+        videoContainerView.clipsToBounds = true
+        lessonThemeLabel.configureCustomLabel(font: .anonymousProBold, fontSize: 14, textColor: .primary, nil)
+        
+        downloadButton.configureButton(title: "Скачати презентацію", imageName: nil,
+                                       fontName: CustomFonts.anonymousProBold.rawValue,
+                                       fontSize: 14,
+                                       tintColor: .primary)
+        telegramButton.configureButton(title: "Telegram канал з матеріалами ", imageName: nil,
+                                       fontName: CustomFonts.anonymousProBold.rawValue,
+                                       fontSize: 14,
+                                       tintColor: .primary)
     }
  
 
